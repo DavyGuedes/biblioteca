6194c82c9cb8e9cba90fc8a74cc0c7095fad4b6b
Bruno Barbosa <barbosa.developer@gmail.com>
2018-08-18 16:58:39 -0300

    Fix coluna categoria do livro

--- src/main/java/br/com/frameworks/biblioteca/dao/LivroDao.java
+++ src/main/java/br/com/frameworks/biblioteca/dao/LivroDao.java
@@ -22,7 +22,7 @@ public class LivroDao implements GenericDao<Livro> {
     @Override
     public boolean adiciona(Livro entity) throws SQLException {
         boolean result;
-        String sql = "insert into livro (titulo, autor, volume, ano, descricao, qtd_exemplares, status_livro, categoria) values (?,?,?,?,?,?,?,?)";
+        String sql = "insert into livro (titulo, autor, volume, ano, descricao, qtd_exemplares, status_livro, categoria_id) values (?,?,?,?,?,?,?,?)";
 
         PreparedStatement ps = connection.prepareStatement(sql);
         ps.setString(1, entity.getTitulo());
@@ -42,7 +42,7 @@ public class LivroDao implements GenericDao<Livro> {
     @Override
     public boolean altera(Livro entity) throws SQLException {
         boolean result;
-        String sql = "update livro set titulo = ?, autor = ?, volume = ?, ano = ?, descricao = ?, qtd_exemplares = ?, status_livro = ?, categoria = ? where id = ?";
+        String sql = "update livro set titulo = ?, autor = ?, volume = ?, ano = ?, descricao = ?, qtd_exemplares = ?, status_livro = ?, categoria_id = ? where id = ?";
 
         PreparedStatement ps = connection.prepareStatement(sql);
         ps.setString(1, entity.getTitulo());
@@ -117,7 +117,7 @@ public class LivroDao implements GenericDao<Livro> {
         String descricao = rs.getString("descricao");
         Integer qtdExemplares = rs.getInt("qtd_exemplares");
         StatusLivro statusLivro = StatusLivro.valueOf(rs.getString("status_livro"));
-        Categoria categoria = categoriaDao.getEntity(rs.getLong("categoria"));
+        Categoria categoria = categoriaDao.getEntity(rs.getLong("categoria_id"));
 
         return new Livro(idLivro, titulo, autor, volume, ano, descricao, qtdExemplares, statusLivro, categoria);
     }
